71832f9f10ee47d72f71542424bb2aec05d9a4aa
Arui <894683425@qq.com>
2026-03-12 18:56:44 +0800

    fix: avoid setting multiple handlers to a single logger

--- utils/logger.py
+++ utils/logger.py
@@ -4,14 +4,19 @@ import os
 class Logger:
     def __init__(self, name: str):
         self.logger = logging.getLogger(name)
+
+        if self.logger.handlers:
+            return
+        
         self.logger.setLevel(logging.INFO)
         handler = logging.StreamHandler()
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
-        if not os.path.exists('logs'):
-            os.makedirs('logs', exist_ok=True)
+
+        os.makedirs('logs', exist_ok=True)
         file_handler = logging.FileHandler(f'logs/{name}.log')
         file_handler.setFormatter(formatter)
+        
         self.logger.addHandler(handler)
         self.logger.addHandler(file_handler)
 
