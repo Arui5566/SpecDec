5a5fea5949e5b8848f8f0a03b2b35eee68886be5
Arui <894683425@qq.com>
2026-03-12 16:12:48 +0800

    add more configurations

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
+draft_model_name = "Qwen/Qwen2.5-1.5B-Instruct"
+target_model_name = "Qwen/Qwen2.5-7B-Instruct"
+
 eos_tokens_id = 151645
+
+prompt = "Translate the following Chinese text to English: '把这句话翻译成英文。'"
