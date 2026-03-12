04c8f34616195c629d7686745540cb90b5e765a3
Arui <894683425@qq.com>
2026-03-12 14:46:20 +0800

    add logger to visualize function info

---
+++ utils/logger.py
@@ -0,0 +1,31 @@
+import logging
+import os
+
+class Logger:
+    def __init__(self, name: str):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        if not os.path.exists('logs'):
+            os.makedirs('logs', exist_ok=True)
+        file_handler = logging.FileHandler(f'logs/{name}.log')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.addHandler(file_handler)
+
+    def debug(self, message: str):
+        self.logger.debug(message)
+
+    def info(self, message: str):
+        self.logger.info(message)
+
+    def warning(self, message: str):
+        self.logger.warning(message)
+
+    def error(self, message: str):
+        self.logger.error(message)
+
+    def critical(self, message: str):
+        self.logger.critical(message)
