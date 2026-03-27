8f8209126698ce1d2676c4ef76d9620594049a40
Arui <894683425@qq.com>
2026-03-27 17:22:50 +0800

    add mllm and change prompt

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 draft_model_name = "Qwen/Qwen2.5-1.5B-Instruct"
 target_model_name = "Qwen/Qwen2.5-7B-Instruct"
 
+draft_mllm_name = "Qwen/Qwen2.5-VL-3B-Instruct"
+target_mllm_name = "Qwen/Qwen2.5-VL-7B-Instruct"
+
 eos_tokens_id = 151645
 
-prompt = "Translate the following Chinese text to English: '把这句话翻译成英文。'"
+prompt = "Describe this image."
